f1ad2e0b0f5b4a2640022a3bf9936ead4ecaa9f9
Rayhona <rayhona.jalilova@mail.ru>
2025-01-04 21:14:47 +0300

    glonal.json, program.cs,HelloController have created now.

---
+++ FirstProject/MyAspNetCoreApp/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
